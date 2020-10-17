4d4b89293dba7beca44118e8a7fb1be8962b6180
melikekurt <melikekurt315@gmail.com>
2020-10-17 17:01:12 +0300

    create km.mil.c

---
+++ km.mil.c
@@ -0,0 +1,19 @@
+//Bu program km cinsi uzunluðu mil cinsine çevirmeye yarar
+#include<stdio.h>
+int main(){
+ float km , mil ;
+  printf("Lutfen km cinsinden bir uzunluk giriniz\n");
+  scanf("%f",&km);
+  mil=km/1.609;
+  printf("%f km %f mile esittir",km,mil);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0 ;
+}
